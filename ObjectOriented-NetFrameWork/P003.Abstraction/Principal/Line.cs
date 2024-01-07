8b3eed46709398126e401b75326cc4aa3bada612
tahirjadoon <tjadoon@gmail.com>
2024-01-07 00:35:39 -0500

    P003.Abstraction

---
+++ ObjectOriented-NetFrameWork/P003.Abstraction/Principal/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P003.Abstraction.Principal
+{
+    internal class Line : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Hi from the draw method in line class");
+        }
+    }
+}
---
+++ ObjectOriented-NetFrameWork/P003.Abstraction/Program.cs
@@ -0,0 +1,20 @@
+﻿using P003.Abstraction.Principal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P003.Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = new Line();
+
+            line.SayHi();
+            line.Draw();
+        }
+    }
+}
