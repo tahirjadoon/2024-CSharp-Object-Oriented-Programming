878ff634dbb2c9adb00c1f953c65ca75c9d00f37
tahirjadoon <tjadoon@gmail.com>
2024-01-09 22:27:22 -0500

    P011.Collections_Stack

---
+++ ObjectOriented-NetFrameWork/P011.Collections_Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using OOP.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P011.Collections_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.WriteLine("Creating and adding to Stack");
+
+            Stack stack = new Stack();
+
+            // Adding
+            stack.Push("David");
+            stack.Push("Emmy");
+            stack.Push("Jack");
+            stack.Push("Emma");
+
+            PrintStack(stack);
+
+            // Removing
+            ConsoleHelper.WriteLine("Removing - Pop two items. Returns the item popped");
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            PrintStack(stack);
+        }
+
+        static void PrintStack(Stack st)
+        {
+            foreach (var elem in st)
+            {
+                ConsoleHelper.WriteLine(elem.ToString(), ConsoleColor.Yellow);
+            }
+        }
+    }
+}
