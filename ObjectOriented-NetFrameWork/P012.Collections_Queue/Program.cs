ee53b64b3d8d292d4ba5175e9059be29938c3681
tahirjadoon <tjadoon@gmail.com>
2024-01-09 22:38:43 -0500

    P012.Collections_Queue

---
+++ ObjectOriented-NetFrameWork/P012.Collections_Queue/Program.cs
@@ -0,0 +1,35 @@
+﻿using OOP.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P012.Collections_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleHelper.WriteLine("Creating and adding to Queue");
+            // Creating
+            Queue queue = new Queue();
+            // Adding
+            queue.Enqueue("Cavin");
+            queue.Enqueue("Tom");
+            queue.Enqueue("Emma");
+
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Removing
+            ConsoleHelper.WriteLine("Removing from Queue");
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
