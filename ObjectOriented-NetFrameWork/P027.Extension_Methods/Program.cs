95dffa39b0b90b51645cb0c0f52543d215bb5b03
tahirjadoon <tjadoon@gmail.com>
2024-01-14 10:19:38 -0500

    P027.Extension_Methods

---
+++ ObjectOriented-NetFrameWork/P027.Extension_Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P027.Extension_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine(x.IsGreater(10));
+
+            string text = "100";
+            Console.WriteLine(text.IsNumber());
+
+            DateTime myDate = new DateTime(2024, 1, 14);
+            Console.WriteLine(myDate.MyCustomFormat());
+        }
+    }
+
+    static class MyCustomExtension
+    {
+        public static bool IsGreater(this int value, int number)
+        {
+            return value > number;
+        }
+
+        public static bool IsNumber(this string text)
+        {
+            return int.TryParse(text, out int result);
+        }
+
+        public static string MyCustomFormat(this DateTime date)
+        {
+            return string.Format("{0:dddd dd \nMMMM yyyy}", date);
+        }
+    }
+}
