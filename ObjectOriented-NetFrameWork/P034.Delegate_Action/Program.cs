013447f290a8f6b6b8e287fd3342a6a87f9faf5f
tahirjadoon <tjadoon@gmail.com>
2024-01-14 22:44:42 -0500

    P034.Delegate_Action

---
+++ ObjectOriented-NetFrameWork/P034.Delegate_Action/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P034.Delegate_Action
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> action1 = DisplayInformation;
+            action1(16);
+
+            Action action2 = DisplayInformation;
+            action2();
+;        }
+
+        public static void DisplayInformation(int number)
+        {
+            Console.WriteLine($"The info with number as {number}");
+        }
+
+        public static void DisplayInformation()
+        {
+            Console.WriteLine("The info without in number");
+        }
+    }
+}
