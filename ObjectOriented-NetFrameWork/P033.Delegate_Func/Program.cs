bff4201630cc4303c86a18963dc3dfea16eb81ff
tahirjadoon <tjadoon@gmail.com>
2024-01-14 22:37:31 -0500

    P033.Delegate_Func

---
+++ ObjectOriented-NetFrameWork/P033.Delegate_Func/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P033.Delegate_Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //the last one is the out parameter
+            //caling the first AddTwoNumbers that recieves two int params
+            Func<int, int, int> funcOne = AddTwoNumbers;
+            Console.WriteLine("funcOne = " + funcOne(3, 10));
+
+            //calling the second AddTwoNumber with no in params
+            Func<int> funcTwo = AddTwoNumbers;
+            Console.WriteLine("funcTwo = " + funcTwo());
+        }
+
+        public static int AddTwoNumbers(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int AddTwoNumbers()
+        {
+            int x = 5;
+            int y = 2;
+            int z = x + y;
+            return z;
+        }
+    }
+}
