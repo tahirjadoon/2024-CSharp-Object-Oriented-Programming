3071aa606c104b794e7a733d7f4e1ccbcea97739
tahirjadoon <tjadoon@gmail.com>
2024-01-14 23:16:58 -0500

    P037.Delegate_Action_With_AnnonymousMethod_LambdaExpression

---
+++ ObjectOriented-NetFrameWork/P037.Delegate_Action_AnnonMethod_LambExp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P037.Delegate_Action_AnnonMethod_LambExp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jack";
+
+            //action
+            Action<string> message = ShowMessage;
+            message(name);
+
+            //Annonymous method
+            Action<string> anonymousMethod = delegate (string inName) { Console.WriteLine($"Hi, how are you today '{inName}'? I am anonymousMethod"); };
+            anonymousMethod(name);
+
+            //lambda Expression
+            Action<string> lambdaExpression = inName => { Console.WriteLine($"Hi, how are you today '{inName}'? I am lambda expression"); };
+            lambdaExpression(name);
+        }
+
+        /// <summary>
+        /// Show message receives name and displays message
+        /// </summary>
+        /// <param name="name"></param>
+        public static void ShowMessage(string name) => Console.WriteLine($"Hi, how are you today {name}? I am action");
+    }
+}
