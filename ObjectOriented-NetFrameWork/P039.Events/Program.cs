ad3144f25f0f222b946df27d045bb0527437f050
tahirjadoon <tjadoon@gmail.com>
2024-01-15 21:07:50 -0500

    P039.Events

---
+++ ObjectOriented-NetFrameWork/P039.Events/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P039.Events
+{
+    public delegate void myDelegate(string name);
+
+    internal class Program
+    {
+        public event myDelegate myEvent;
+
+        public Program()
+        {
+            myEvent += new myDelegate(SayHiEnglish);
+            myEvent += new myDelegate(SayHiArabic);
+            myEvent += new myDelegate(SayHiItalian);
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.myEvent("Jack");
+        }
+
+        static void SayHiEnglish(string name) => Console.WriteLine($"Hi {name}");
+
+        static void SayHiArabic(string name) => Console.WriteLine($"Marhaaban {name}");
+
+        static void SayHiItalian(string name) => Console.WriteLine($"Caio {name}");
+
+    }
+}
