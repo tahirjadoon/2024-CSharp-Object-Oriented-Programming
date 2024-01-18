11aadb581e0768e735c75a675d6f6060d9f1015d
tahirjadoon <tjadoon@gmail.com>
2024-01-17 22:13:11 -0500

    P048.Semaphore

---
+++ ObjectOriented-NetFrameWork/P048.Semaphore/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P048.Semaphore
+{
+    internal class Program
+    {
+        private static System.Threading.Semaphore semaphore = new System.Threading.Semaphore(2, 2);
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(delegate ()
+                {
+                    for (int d = 0; d < 2; d++)
+                    {
+                        UseResource(d);
+                    }
+                }));
+                newThread.Name = String.Format("(Thread no. {0})", i + 1);
+                newThread.Start();
+            }
+        }
+
+        private static void UseResource(int x)
+        {
+            Console.WriteLine("{0} is requesting the enter for d={1}", Thread.CurrentThread.Name, x);
+            semaphore.WaitOne();
+
+            Console.WriteLine("{0} has entered the critical section for d={1}", Thread.CurrentThread.Name, x);
+            Thread.Sleep(10000);
+            Console.WriteLine("{0} is leaving the critical section for d={1}", Thread.CurrentThread.Name, x);
+
+            semaphore.Release();
+            Console.WriteLine("{0} has left the critical section for d={1}", Thread.CurrentThread.Name, x);
+        }
+    }
+}
