2a2ade19bc26e08aba4f3f32d213f4a0f4f7baf6
tahirjadoon <tjadoon@gmail.com>
2024-01-17 21:39:41 -0500

    P044.Deadlocks_Lock_Keyword

---
+++ ObjectOriented-NetFrameWork/P044.Deadlocks_Lock_Keyword/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P044.Deadlocks_Lock_Keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Files file = new Files();
+            Thread[] threads = new Thread[10];
+
+            for (int i = 0; i < 10; i++) 
+            {
+                threads[i] = new Thread(new ParameterizedThreadStart(file.Write));
+                threads[i].Start($"c:\\accounts{i}.txt");
+            }
+        }
+
+        class Files
+        {
+            public object fileLock = new object();
+
+            public void Write(object path)
+            {
+                lock (fileLock)
+                {
+                    Console.WriteLine($"Write in {path}");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Writing process has been completed\n");
+                }
+            }
+
+            public void Read(object path)
+            {
+                lock (fileLock)
+                {
+                    Console.WriteLine($"Read from {path}");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Reading process has been completed\n");
+                }
+            }
+        }
+    }
+}
