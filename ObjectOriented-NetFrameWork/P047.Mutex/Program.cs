7ac7bd8cb51e02151a421cdaf71a3343fc8b4d6c
tahirjadoon <tjadoon@gmail.com>
2024-01-17 22:04:31 -0500

    P047.Mutex

---
+++ ObjectOriented-NetFrameWork/P047.Mutex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P047.Mutex
+{
+    internal class Program
+    {
+        private static System.Threading.Mutex mut = new System.Threading.Mutex();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread newThread = new Thread(new ThreadStart(delegate ()
+                {
+                    for (int d = 0; d < 2; d++)
+                    {
+                        UseResource(d);
+                    }
+                }));
+                newThread.Name = String.Format("(Thread no. {0})", i + 1);
+                newThread.Start();
+            }
+        }
+
+        private static void UseResource(int x)
+        {
+            Console.WriteLine("{0} is requesting the mutex for d={1}", Thread.CurrentThread.Name, x);
+            mut.WaitOne();
+
+            Console.WriteLine("{0} has entered the critical section for d={1}", Thread.CurrentThread.Name, x);
+            Thread.Sleep(10000);
+            Console.WriteLine("{0} is leaving the critical section for d={1}", Thread.CurrentThread.Name, x);
+
+            mut.ReleaseMutex();
+            Console.WriteLine("{0} has released the mutex for d={1}", Thread.CurrentThread.Name, x);
+        }
+    }
+}
