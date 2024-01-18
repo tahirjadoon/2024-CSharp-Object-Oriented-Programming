7950469ea46f0ac5f644c13a4fb3423c05549697
tahirjadoon <tjadoon@gmail.com>
2024-01-17 21:44:08 -0500

    P045.Parallel_Threads

---
+++ ObjectOriented-NetFrameWork/P045.Parallel_Threads/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P045.Parallel_Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+            () => {
+                PrintCount(-100, 0, "negative");
+            },
+            () => {
+                PrintCount(1, 100, "positive");
+            }
+        );
+        }
+
+        static void PrintCount(int LowLimit, int UpperLimit, string text)
+        {
+            for (int i = LowLimit; i <= UpperLimit; i++)
+            {
+                Console.WriteLine($"> {text} {i}");
+            }
+        }
+    }
+}
