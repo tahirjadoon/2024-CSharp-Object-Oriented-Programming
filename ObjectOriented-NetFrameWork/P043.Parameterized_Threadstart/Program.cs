6ac3c18aec98eb4424d0ae12017c5c38e82213d4
tahirjadoon <tjadoon@gmail.com>
2024-01-17 21:18:51 -0500

    P043.Parameterized_Threadstart

---
+++ ObjectOriented-NetFrameWork/P043.Parameterized_Threadstart/Program.cs
@@ -0,0 +1,31 @@
+﻿using OOP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P043.Parameterized_Threadstart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            Thread t1 = new Thread(new ParameterizedThreadStart(helper.Loop));
+            t1.Start(15);
+        }
+
+        class Helper
+        {
+            public void Loop(object number)
+            {
+                for (int i = 0; i<(int)number; i++)
+                {
+                    ConsoleHelper.WriteLine(((int)i).ToString(), ConsoleColor.Yellow);
+                }
+            }
+        }
+    }
+}
