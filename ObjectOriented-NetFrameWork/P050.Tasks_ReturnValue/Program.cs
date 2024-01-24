c3aca0efcc003389267ca268bdaaaa5d5dfd1332
tahirjadoon <tjadoon@gmail.com>
2024-01-23 22:02:44 -0500

    P050.Tasks_ReturnValue

---
+++ ObjectOriented-NetFrameWork/P050.Tasks_ReturnValue/Program.cs
@@ -0,0 +1,32 @@
+﻿using OOP.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P050.Tasks_ReturnValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = new Task<int>(() =>
+            {
+                int result = 0;
+                for(int i = 1; i <= 5; i++)
+                {
+                    result += i;
+                    ConsoleHelper.WriteLine($"{i}: result = {result}", ConsoleColor.Yellow);
+                }
+                return result;
+            });
+            task.Start();
+
+            ConsoleHelper.WriteLineLock($"Return value for result: {task.Result}", ConsoleColor.Green);
+
+            Console.ReadLine();
+
+        }
+    }
+}
