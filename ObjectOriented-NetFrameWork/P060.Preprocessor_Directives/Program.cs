f5f7fc7e0eeded3d4047f383d882df15b0e3efbc
tahirjadoon <tjadoon@gmail.com>
2024-01-29 20:42:13 -0500

    P060.Preprocessor_Directives

---
+++ ObjectOriented-NetFrameWork/P060.Preprocessor_Directives/Program.cs
@@ -0,0 +1,29 @@
+﻿
+#define underDev
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P060.Preprocessor_Directives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region First Block
+            var x = 20;
+            Console.WriteLine($"Firts region: {x}");
+            #endregion First end
+
+#if (underDev)
+            Console.WriteLine($"This app is under development");
+#else
+            Console.WriteLine($"This app is live");
+#endif
+        }
+    }
+}
