12e1084254689c9b7dab83a7d150a8ce215cab86
tahirjadoon <tjadoon@gmail.com>
2024-01-29 20:30:51 -0500

    P059.Static_Directives

---
+++ ObjectOriented-NetFrameWork/P059.Static_Directives/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Math;
+using static System.Console;
+
+namespace P059.Static_Directives
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(Round(2.54667, 2));
+        }
+    }
+}
