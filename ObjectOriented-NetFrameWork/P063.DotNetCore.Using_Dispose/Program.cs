2b002799ca235d2af7f85239e1dfb6c7b6766b0f
tahirjadoon <tjadoon@gmail.com>
2024-01-30 22:19:59 -0500

    P063.DotNetCore.Using_Dispose

---
+++ ObjectOriented-NetFrameWork/P063.DotNetCore.Using_Dispose/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP.DotNetCore.Core;
+
+using var resource = new Resource();
+resource.Use();
+
+ConsoleHelper.WriteLine("This app is awesome", ConsoleColor.Cyan);
+
+Console.ReadLine();
+
+
+class Resource: IDisposable
+{
+    public void Use() => ConsoleHelper.WriteLine("Using resource...", ConsoleColor.Yellow);
+
+    public void Dispose() => ConsoleHelper.WriteLine("Disposing resource", ConsoleColor.Red);
+}
