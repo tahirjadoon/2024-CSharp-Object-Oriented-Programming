b27a15d624bd3606740fa8a541bb24b48b13a3dc
tahirjadoon <tjadoon@gmail.com>
2024-01-30 23:17:44 -0500

    P066.DotNetCore.PropertyPattern_SwitchExp

---
+++ ObjectOriented-NetFrameWork/P066.DotNetCore.PropertyPattern_SwitchExp/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var viennaAus = new City(Cities.Vienna, "Austria");
+var parisFr = new City(Cities.Paris, "France");
+var parisUS = new City(Cities.Paris, "US");
+
+Console.WriteLine(CheckPrices(viennaAus));
+Console.WriteLine(CheckPrices(parisFr));
+Console.WriteLine(CheckPrices(parisUS));
+
+
+static string CheckPrices(City location) =>
+    location switch
+    {
+        { CityName: Cities.Warsaw } => "Reasonable",
+        { CityName: Cities.Budapest } => "Reasonable",
+        { CityName: Cities.Vienna } => "Above Average",
+        { CityName: Cities.Paris, CountryName: "US" } => $"{location.CityName}, {location.CountryName} Reasonable",
+        { CityName: Cities.Paris, CountryName: "France" } => "High",
+
+        _ => "Invalid Input"
+    };
+
+public enum Cities
+{
+    Vienna,
+    Berlin,
+    Amsterdam,
+    Madrid,
+    Budapest,
+    Paris,
+    London,
+    Warsaw,
+    Prague,
+    Melbourne
+}
+
+public class City
+{
+    private readonly Cities cityName;
+    private readonly string countryName;
+
+    public City(Cities city, string country)
+    {
+        cityName = city;
+        countryName = country;
+    }
+
+    public Cities CityName => cityName;
+    public string CountryName => countryName;
+
+    
+}
