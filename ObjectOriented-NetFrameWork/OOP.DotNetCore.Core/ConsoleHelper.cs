b9461cd4e6c1545b681fc5760504a3cd5a6e0250
tahirjadoon <tjadoon@gmail.com>
2024-01-30 21:46:08 -0500

    P062.DotNetCore.Default_Interface_Methods

---
+++ ObjectOriented-NetFrameWork/OOP.DotNetCore.Core/ConsoleHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace OOP.DotNetCore.Core;
+
+    public class ConsoleHelper
+    {
+        static object writeLock = new object();
+
+        public static void WriteLine(string message)
+        {
+            WriteLine(message, null);
+        }
+
+        public static void WriteLine(string message, ConsoleColor? color)
+        {
+            var defaultColor = Console.ForegroundColor;
+            if (color != null)
+            {
+                Console.ForegroundColor = color.GetValueOrDefault();
+            }
+            Console.WriteLine(message);
+            if (color != null)
+            {
+                Console.ResetColor();// = defaultColor;
+            }
+        }
+        public static void WriteLineLock(string message)
+        {
+            WriteLineLock(message, null);
+        }
+
+        public static void WriteLineLock(string message, ConsoleColor? color)
+        {
+            lock (writeLock)
+            {
+                WriteLine(message, color);
+            }
+
+        }
+    }
+
---
+++ ObjectOriented-NetFrameWork/P062.DotNetCore.Default_Interface_Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OOP.DotNetCore.Core;
+
+ICar f1 = new Ferrari();
+f1.Start();
+f1.Run();
+f1.Run(45);
+
+ConsoleHelper.WriteLine("\n---------\n", ConsoleColor.Red);
+
+var f2 = new Ferrari();
+((ICar)f2).Start();
+f2.Run();
+((ICar)f2).Run(55);
+
+Console.ReadLine();
+
+class Ferrari : ICar
+{
+    public void Run()
+    {
+        Console.WriteLine("Car is running");
+    }
+}
+
+/// <summary>
+/// ICar interface with default methods
+/// </summary>
+interface ICar
+{
+    public void Start() => Console.WriteLine("Car has started");
+
+    public void Run();
+
+    public void Run(double speed) => Console.WriteLine($"Car is running at {speed}");
+}
