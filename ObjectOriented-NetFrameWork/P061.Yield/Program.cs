d2d6dbb962207ba465ea510f80ed2edd9da8454d
tahirjadoon <tjadoon@gmail.com>
2024-01-30 20:56:25 -0500

    P061.Yield

---
+++ ObjectOriented-NetFrameWork/P061.Yield/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P061.Yield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in PerformMathOperation(3,2))
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static IEnumerable<double> PerformMathOperation(int one, int two)
+        {
+            yield return one + two;
+            yield return one - two;
+            yield return one * two;
+            yield return Math.Round(((double)one / (double)two), 2);
+        }
+    }
+}
